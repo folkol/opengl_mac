a60847e2cb9b09b8a905861bedef63f2fc0c2338
Mattias Johansson <mattias.johansson@atex.com>
2014-10-09 15:45:05 +0200

    Added menu with quit item

--- main.m
+++ main.m
@@ -2,11 +2,23 @@
 #include <AppKit/NSWindow.h>
 #include <AppKit/NSMenu.h>
 
-// cc -framework AppKit -x objective-c main.m
 void create_window() {
   @autoreleasepool {
     [NSApplication sharedApplication];
     [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+
+    id menubar = [[NSMenu new] autorelease];
+    id appMenuItem = [[NSMenuItem new] autorelease];
+    [menubar addItem:appMenuItem];
+    [NSApp setMainMenu:menubar];
+    id appMenu = [[NSMenu new] autorelease];
+    id appName = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleName"];
+    id quitTitle = [@"Quit " stringByAppendingString:appName];
+    id quitMenuItem = [[[NSMenuItem alloc] initWithTitle:quitTitle
+                                           action:@selector(terminate:)
+                                           keyEquivalent:@"q"] autorelease];
+    [appMenu addItem:quitMenuItem];
+    [appMenuItem setSubmenu:appMenu];
 
     NSRect frame = NSMakeRect(100, 100, 500, 500);
     NSWindow* window  = [[[NSWindow alloc] initWithContentRect:frame
