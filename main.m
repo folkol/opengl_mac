8caf0d1e067b72c7a7816c5ceeb4a57057e6a317
Mattias Johansson <mattias.johansson@atex.com>
2014-10-09 13:42:35 +0200

    Moved the window a bit

--- main.m
+++ main.m
@@ -1,19 +1,22 @@
 #include <AppKit/NSApplication.h>
 #include <AppKit/NSWindow.h>
 #include <AppKit/NSColor.h>
+#include <AppKit/NSMenu.h>
 
 // cc -framework AppKit -x objective-c main.m
 void create_window() {
   @autoreleasepool {
     [NSApplication sharedApplication];
     [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
-    NSRect frame = NSMakeRect(0, 0, 200, 200);
+
+    NSRect frame = NSMakeRect(100, 100, 500, 500);
     NSWindow* window  = [[[NSWindow alloc] initWithContentRect:frame
                                            styleMask:NSBorderlessWindowMask
                                            backing:NSBackingStoreBuffered
                                            defer:NO] autorelease];
+
     [window makeKeyAndOrderFront:NSApp];
-    
+
     [NSApp activateIgnoringOtherApps:YES];
     [NSApp run];
   }
